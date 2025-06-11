a8f211ba15937cccc3ef5146264c36b24ec44508
Jason Beres <jasonberes@gmail.com>
2025-06-11 19:08:50 -0400

    Add files via upload

---
+++ Reveal/DataSourceProvider.cs
@@ -0,0 +1,62 @@
+﻿using Reveal.Sdk;
+using Reveal.Sdk.Data;
+using Reveal.Sdk.Data.MySql;
+
+namespace RevealSdk.Server.Reveal
+
+{
+    internal class DataSourceProvider : IRVDataSourceProvider
+    {
+        public Task<RVDataSourceItem> ChangeDataSourceItemAsync(IRVUserContext userContext, string dashboardId, RVDataSourceItem dataSourceItem)
+        {
+            if (dataSourceItem is RVMySqlDataSourceItem sqlDsi)
+            {
+                ChangeDataSourceAsync(userContext, sqlDsi.DataSource);
+
+                switch (sqlDsi.Id)
+                {
+                    // ****
+                    // Example of an Stored Procedure with a customerId parameter from the userContext
+                    // ****
+                    case "sp_customer_orders":
+                        sqlDsi.Procedure = sqlDsi.Id;
+                        sqlDsi.ProcedureParameters = new Dictionary<string, object> { { "customer", userContext.UserId } };
+                        break;
+                    // ****
+                    // Example of an ad-hoc query with a customerId parameter from the userContext
+                    // ****
+                    case "customer_orders":
+                        sqlDsi.CustomQuery = $"select * from {sqlDsi.Id} where customer_id = {userContext.UserId}";
+                        break;
+                    // ****
+                    // Example of a ad-hoc query to a View with a orderId parameter from the userContext
+                    // ****
+                    case "customer_orders_details":
+                        sqlDsi.CustomQuery = $"select * from {sqlDsi.Id} where order_id = {userContext.Properties["OrderId"]}";
+                        break;
+                    // ****
+                    // This assumes the Data Sources Dialog table / object is selected
+                    // you can check the incoming request of function, table to customize the query
+                    // ****
+                    default:
+                        if (sqlDsi.Table == "customers")
+                        { 
+                            sqlDsi.CustomQuery = $"select * from {sqlDsi.Table} where id = {userContext.UserId}";
+                        }
+                        break;
+                }
+            }
+            return Task.FromResult(dataSourceItem);
+        }
+
+        public Task<RVDashboardDataSource> ChangeDataSourceAsync(IRVUserContext userContext, RVDashboardDataSource dataSource)
+        {
+            if (dataSource is RVMySqlDataSource sqlDs)
+            {
+                sqlDs.Host = "infragistics.local";
+                sqlDs.Database = "northwind";             
+            }
+            return Task.FromResult(dataSource);
+        }
+    }
+}
---
+++ Reveal/UserContextProvider.cs
@@ -0,0 +1,60 @@
+﻿using Reveal.Sdk;
+using System.Text.RegularExpressions;
+
+namespace RevealSdk.Server.Reveal
+{
+    public class UserContextProvider : IRVUserContextProvider
+    {
+        IRVUserContext IRVUserContextProvider.GetUserContext(HttpContext aspnetContext)
+        {
+
+            // ****
+            // In this case, there are 2 headers sent in clear text to the server
+            // Normally, you'd be accepting your token or other secrets that you'd use 
+            // for the security context of your data requests,
+            // or you would be passing query parameters for custom queries, etc.
+            // ****
+
+            var userIdString = aspnetContext.Request.Headers["x-header-one"];
+            var orderId = aspnetContext.Request.Headers["x-header-two"];
+
+            int userId;
+
+            if (string.IsNullOrEmpty(userIdString) || userIdString == "0")
+            {
+                // this is for testing purposes only to ensure there is a valid userId
+                userId = 3; // Assign the default value, this is an Admin user in the ObjectFilter
+            }
+            else if (!int.TryParse(userIdString, out userId) || !IsValidCustomerId(userId))
+            {
+                throw new ArgumentException("Invalid CustomerID format. CustomerID must be a valid integer between 1 and 30.");
+            }
+
+            // At this point, 'userId' will be either the parsed valid ID or the default value of 3.
+            // You can now use the 'userId' variable in the rest of your code.
+
+            // Proceed with valid userId
+
+            // ****
+            // Set up Roles based on the incoming user id.  In a real app, this would be set up to match
+            // your scenario and be dynamically loaded
+            // ****
+            string role = "User";
+            if (userIdString == "3" || userIdString == "11")
+            {
+                role = "Admin";
+            }
+
+            // ****
+            // Create an array of properties that can be used in other Reveal functions
+            // ****
+            var props = new Dictionary<string, object>() {
+                    { "OrderId", orderId },
+                    { "Role", role } };
+
+            return new RVUserContext(userIdString, props);
+        }
+
+        private static bool IsValidCustomerId(int customerId) => customerId >= 1 && customerId <= 30;
+    }
+}
